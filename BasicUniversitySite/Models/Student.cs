73e914630e5b3e68f45fa8c82ccbd14ea87d4919
thefenry <thefenry@gmail.com>
2015-03-15 22:20:20 -0700

    Added more models that make the relations complex
    The Models now have many to many and one to many. The data migration are
    set and we can proceed to reading and updating these models

--- BasicUniversitySite/Models/Student.cs
+++ BasicUniversitySite/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,29 @@ namespace BasicUniversitySite.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength=1)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage="First name cannot be longer than 50 characters.")]
+        [RegularExpression(@"[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Column("FirstName")]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        [Display(Name="EnrollmentDate")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
